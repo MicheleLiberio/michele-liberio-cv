c7d614f421516e671bd063616f9251cd36286e99
Michele Liberio <michele.liberio@macnil.it>
2026-05-08 21:17:37 +0200

    commit Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/michele-liberio-cv/',
+  //base: '/michele-liberio-cv/',
   plugins: [react(), tailwindcss()]
 })
